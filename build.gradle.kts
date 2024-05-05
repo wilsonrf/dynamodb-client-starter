a34a39154f116ef42c25a5a30f2cd3985a2dfe5a
Wilson da Rocha Franca <wilsonrf@gmail.com>
2024-05-05 18:06:25 +0200

    code setup

---
+++ build.gradle.kts
@@ -0,0 +1,58 @@
+/*
+* Copyright 2024 Wilson da Rocha França
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+ */
+plugins {
+    id("java-library")
+    id("org.springframework.boot") apply false
+    id("io.spring.dependency-management")
+    id("com.github.ben-manes.versions")
+    id("maven-publish")
+}
+
+group = "com.wilsonfranca"
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+dependencyManagement {
+    imports {
+        mavenBom(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES)
+    }
+}
+dependencies {
+    api("com.wilsonfranca:dynamodb-client-autoconfigure:1.0.0-SNAPSHOT")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+rootProject.name = "dynamodb-client-starter"
+
+pluginManagement {
+    val springBootVersion: String by settings
+    val springDependencyManagementVersion: String by settings
+    val dependencyManagementVersion: String by settings
+
+    plugins {
+        id("org.springframework.boot") version springBootVersion
+        id("io.spring.dependency-management") version springDependencyManagementVersion
+        id("com.github.ben-manes.versions") version dependencyManagementVersion
+    }
+}
